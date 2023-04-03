4f1e6ac0710219b0486daa25a6de120bba540165
LookAtYourSkill <callmesleeplxss@gmail.com>
2023-04-03 02:36:16 +0200

    password utilities

--- utils/password.py
+++ utils/password.py
@@ -16,23 +16,23 @@ def checkPasswordLimitation(password):
         # raise exception if password is shorter than 8 characters
         raise Exception("Password is too short")
     
-    # check if password has any uppercase  letters
-    if not any(string.ascii_uppercase() in password):
+    # check if password has any uppercase letters
+    if not any(char in string.ascii_uppercase for char in password):
         # raise exception if password has no uppercase letters
         raise Exception("Password has no uppercase characters")
     
     # check if password has any lowercase letters
-    if not any(string.ascii_lowercase() in password):
+    if not any(char in string.ascii_lowercase for char in password):
         # raise exception if password has no lowercase letters
         raise Exception("Password has no lowercase characters")
     
     # check if password has any numbers
-    if not any(string.digits() in password):
+    if not any(char in string.digits for char in password):
         # raise exception if password has no numbers
         raise Exception("Password has no numbers")
     
     # check if password has any special characters
-    if not any(string.punctuation() in password):
+    if not any(char in string.punctuation for char in password):
         # raise exception if password has no special characters
         raise Exception("Password has no special characters")
     
