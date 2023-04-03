1b24525909761ef6f093ccfb654f11f6a4c9a42e
LookAtYourSkill <callmesleeplxss@gmail.com>
2023-04-03 02:08:16 +0200

    use of hashlib instead of bcrypt

---
+++ utils/password.py
@@ -0,0 +1,41 @@
+import hashlib
+import string
+
+
+def getHashedPassword(password: str):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+def checkPassword(password: str, hashedPassword: str):
+    return getHashedPassword(password) == hashedPassword
+
+
+def checkPasswordLimitation(password):
+    # check if password is longer than 8 characters
+    if len(password) < 8:
+        # raise exception if password is shorter than 8 characters
+        raise Exception("Password is too short")
+    
+    # check if password has any uppercase  letters
+    if not any(string.ascii_uppercase() in password):
+        # raise exception if password has no uppercase letters
+        raise Exception("Password has no uppercase characters")
+    
+    # check if password has any lowercase letters
+    if not any(string.ascii_lowercase() in password):
+        # raise exception if password has no lowercase letters
+        raise Exception("Password has no lowercase characters")
+    
+    # check if password has any numbers
+    if not any(string.digits() in password):
+        # raise exception if password has no numbers
+        raise Exception("Password has no numbers")
+    
+    # check if password has any special characters
+    if not any(string.punctuation() in password):
+        # raise exception if password has no special characters
+        raise Exception("Password has no special characters")
+    
+    else:
+        # return True if password is valid
+        return True
